36d4a8a8b5824829c5e0e5ae429a7581b2ebd85d
Tushar <kmrtushar01@gmail.com>
2023-09-19 23:07:14 +0530

    adding comments

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const getValue = document.querySelector(".modal");
 const getValueOverlay = document.querySelector(".overlay");
 
+
+// Writing open Modal Method
 const openModal = () =>{
     getValue.classList.add("active");
     getValueOverlay.classList.add("removeOverlay");
 };
 
+// writing close Modal Method
 const closeModal = () =>{
     getValue.classList.remove("active");
     getValueOverlay.classList.remove("removeOverlay");
